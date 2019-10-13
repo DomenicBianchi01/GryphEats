c7632f3838dfc58c01fda6460d10bc2400765642
Shuaib Solker <ssolker@uoguelph.ca>
2019-10-12 22:21:51 -0400

    Added users, rest and foods query

--- GryphEats/BackEnd/Apollo/src/index.js
+++ GryphEats/BackEnd/Apollo/src/index.js
@@ -1,46 +1,32 @@
-const { ApolloServer, gql } = require('apollo-server');
+const { ApolloServer } = require('apollo-server');
+const typeDefs = require('./schema');
+const resolvers = require('./resolvers');
+const { createDB } = require('./utils');
 
-// A schema is a collection of type definitions (hence "typeDefs")
-// that together define the "shape" of queries that are executed against
-// your data.
-const typeDefs = gql`
-  # Comments in GraphQL strings (such as this one) start with the hash (#) symbol.
+const GryphAPI = require('./datasources/gryphEats');
+const database = createDB();
 
-  # This "Book" type defines the queryable fields for every book in our data source.
-  type Book {
-    title: String
-    author: String
-  }
+const dataSources = () => ({
+    gryphAPI: new GryphAPI({ database }),
+});
 
-  # The "Query" type is special: it lists all of the available queries that
-  # clients can execute, along with the return type for each. In this
-  # case, the "books" query returns an array of zero or more Books (defined above).
-  type Query {
-    books: [Book]}`
-    ;
+//called everytime something calls Graph
+const context = async ({ req }) => {
+    // simple auth check on every request
+    console.log(req.headers);
+    return "IDK";
+}
 
-const books = [
-    {
-        title: 'Harry Potter and the Chamber of Secrets',
-        author: 'J.K. Rowling',
-    },
-    {
-        title: 'Jurassic Park',
-        author: 'Michael Crichton',
-    },
-];
-
-// Resolvers define the technique for fetching the types defined in the
-// schema. This resolver retrieves books from the "books" array above.
-const resolvers = {
-    Query: {
-        books: () => books,
-    },
-};
-
-// The ApolloServer constructor requires two parameters: your schema
+// The ApolloServer constructor requires two parameters: 
+// your schema = typeDefs
 // definition and your set of resolvers.
-const server = new ApolloServer({ typeDefs, resolvers });
+const server = new ApolloServer({
+    context,
+    typeDefs,
+    resolvers,
+    dataSources,
+    // context: { gryphAPI }
+});
 
 // The `listen` method launches a web server.
 server.listen().then(({ url }) => {
