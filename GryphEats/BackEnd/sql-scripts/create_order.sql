34392c6f94434f0246d080eec8346bfacc76f60b
root <root@CIS4250-F19-1.socs.uoguelph.ca>
2019-10-16 11:50:18 -0400

    changed to date/time sql standards

--- GryphEats/BackEnd/sql-scripts/create_order.sql
+++ GryphEats/BackEnd/sql-scripts/create_order.sql
@@ -1,1 +1,1 @@
-create table foodorder (orderid int not null primary key auto_increment, timeplaced varchar(255) not null, timecompleted varchar(255));
+create table foodorder (orderid int not null primary key auto_increment, timeplaced datetime not null, timecompleted datetime);
