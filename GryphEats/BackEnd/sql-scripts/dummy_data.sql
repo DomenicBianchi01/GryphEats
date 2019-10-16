e7fe042afcf0642aac7b5253a1e2e3aae1d41a82
Scott Riva <sriva@uoguelph.ca>
2019-10-16 12:53:05 -0400

    Sr/encryption

--- GryphEats/BackEnd/sql-scripts/dummy_data.sql
+++ GryphEats/BackEnd/sql-scripts/dummy_data.sql
@@ -5,7 +5,7 @@ insert into restaurant values (null, 'moms kitchen', '905123456', '09:00:00', '2
 select @restaurant_id := restaurantid from restaurant where description = 'TESTDATA';
 select @order_id := orderid from foodorder where timeplaced ='2019-12-31 23:59:59';
 
-insert into user values (null, 'scott', 'riva', '905123456', 'home', 'a@a.com' , 'ewwt', @order_id, @order_id);
+insert into user values (null, 'scott', 'riva', '905123456', 'home', 'a@a.com' , 'ewwt', @order_id, @order_id, 0);
 insert into food values (null, 'chicken fingers', 0, 4.49, @restaurant_id, 1, 'TESTDATA', 1);
 
 select @food_id := foodid from food where description = 'TESTDATA';
